57ee3bfdf3cade0d608d94d9fc383e886a3384a0
collnell <cnell@uci.edu>
2018-07-24 01:47:47 -0400

    try me (#2)

---
+++ dust_functions.R
@@ -0,0 +1,15 @@
+## functions for rdatadust analysis
+
+## split taxa
+# a function that splits otu table output with taxonomy listed all in one column to seprate columns for each grouping
+# provide dataframe and name of column containing the taxonomy list
+split_taxa<-function(df, col='taxonomy'){
+  df$kingdom<-str_match(df[,col], "k__(.*?);")[,2]
+  df$phylum<-str_match(df[,col], "p__(.*?);")[,2]
+  df$class<-str_match(df[,col], "c__(.*?);")[,2]
+  df$order<-str_match(df[,col], "o__(.*?);")[,2]
+  df$family<-str_match(df[,col], "f__(.*?);")[,2]
+  df$genus<-str_match(df[,col], "g__(.*?);")[,2]
+  df$species<-str_match(df[,col], "s__(.*?)")[,2]
+  return(df)
+}
